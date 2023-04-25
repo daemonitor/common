955dac37892f2305336581193875b9ae4e1a9ed3
Larry W <l422y@l422y.com>
2023-04-24 22:39:28 -0400

    Refactor Renderable class to return placeholder text instead of a div with 20 paragraphs

--- Renderable.ts
+++ Renderable.ts
@@ -1,10 +1,11 @@
-export default class Renderable {
+export class Renderable {
     async render(): Promise<any> {
-        return h(
-            "div",
-            Array.from({length: 20}).map(() => {
-                return h("p", "hi")
-            })
-        )
+        // return h(
+        //     "div",
+        //     Array.from({length: 20}).map(() => {
+        //         return h("p", "hi")
+        //     })
+        // )
+        return "..."
     }
 }
